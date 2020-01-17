697e4cc9cbd9b1681c0253dbd92bbc7f4e7cf685
Kevin Tanner <kevindavidtanner@gmail.com>
2020-01-17 10:42:02 -0800

    Adds initial project files

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Kevin Tanner on 1/17/20.
+//  Copyright © 2020 Kevin Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
