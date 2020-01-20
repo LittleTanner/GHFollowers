67e338b1697400d560d5cfb2db2711cdc185ca74
Kevin Tanner <kevindavidtanner@gmail.com>
2020-01-20 11:15:36 -0800

    Sets up custom buttons and textfield objects for subclassing

---
+++ GHFollowers/GFTextField.swift
@@ -0,0 +1,42 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by Kevin Tanner on 1/20/20.
+//  Copyright © 2020 Kevin Tanner. All rights reserved.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        // Testing out Sean Allen's preference of aligning up the "=" signs for readability, unsure how i feel about it
+        layer.cornerRadius          = 10
+        layer.borderWidth           = 2
+        layer.borderColor           = UIColor.systemGray4.cgColor
+        
+        textColor                   = .label
+        tintColor                   = .label
+        textAlignment               = .center
+        font                        = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth   = true
+        minimumFontSize             = 12
+        
+        backgroundColor             = .tertiarySystemBackground
+        autocorrectionType          = .no
+        
+        placeholder                 = "Enter a username"
+    }
+}
