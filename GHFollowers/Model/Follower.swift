2386b2351c8056f496394df7293c9883505681e9
Kevin Tanner <kevindavidtanner@gmail.com>
2020-01-27 13:53:21 -0800

    Creates follower and user models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Kevin Tanner on 1/27/20.
+//  Copyright © 2020 Kevin Tanner. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
