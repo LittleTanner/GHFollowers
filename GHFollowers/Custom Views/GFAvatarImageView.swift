0d1dfe062351153040c70a0fe0aee7947887b7b7
Kevin Tanner <kevindavidtanner@gmail.com>
2020-01-30 11:56:32 -0800

    Adds pagination to get the next 100 followers

--- GHFollowers/Custom Views/GFAvatarImageView.swift
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -41,7 +41,7 @@ class GFAvatarImageView: UIImageView {
         
         guard let url = URL(string: urlString) else { return }
         
-        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
             guard let self = self else { return }
             if error != nil { return }
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
