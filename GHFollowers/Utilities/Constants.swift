81debf9903fd088f9f8ab55cb9f121bd34e5587b
Kevin Tanner <kevindavidtanner@gmail.com>
2020-02-03 16:12:48 -0800

    Sets up header view for the user info view controller

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Kevin Tanner on 2/3/20.
+//  Copyright © 2020 Kevin Tanner. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
